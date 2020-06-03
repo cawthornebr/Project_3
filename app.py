d4f39c8b6c597621cac10d057a3a917b263eb818
Brandon Cawthorne <cawthornebr@gmail.com>
2020-06-02 20:26:20 -0700

    Updated links

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def index():
     print("----------------------")
     return render_template('index.html')
 
+@app.route("/index1")
+def index1():
+    print("----------------------")
+    print("index called")
+    print("----------------------")
+    return render_template('index1.html')
+
 @app.route("/ml")
 def ml():
     print("----------------------")
@@ -19,12 +26,26 @@ def ml():
     print("----------------------")
     return render_template('ML.html')
 
+@app.route("/ml1")
+def ml1():
+    print("----------------------")
+    print("ML called")
+    print("----------------------")
+    return render_template('ML1.html')
+
 @app.route("/unemployment")
 def unemployment():
     print("----------------------")
     print("unemployment called")
     print("----------------------")
-    return render_template('unemployment.html')
+    return render_template('unemployment_map.html')
+
+@app.route("/unemployment1")
+def unemployment1():
+    print("----------------------")
+    print("unemployment called")
+    print("----------------------")
+    return render_template('unemployment _dashboard.html')
 
 @app.route("/other-data")
 def otherdata():
